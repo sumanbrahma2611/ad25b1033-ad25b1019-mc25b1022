76cd639305a229a5686571eb27933046a78303a3
suman Brahma <sumanbrahma2611@gmail.com>
2026-05-30 13:14:18 +0530

    Add files via upload

--- cifar10_cnn_classifier.py
+++ cifar10_cnn_classifier.py
@@ -5,10 +5,7 @@ from tensorflow.keras.utils import to_categorical
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import (
-    precision_score,
-    recall_score,
-    f1_score,
-    classification_report
+    precision_score,recall_score,f1_score,classification_report
 )
 # LOAD DATASET
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -19,40 +16,30 @@ y_test_original = y_test.copy()
 y_train = to_categorical(y_train, 10)
 y_test = to_categorical(y_test, 10)
 
-class_names = [
-    'airplane',
-    'automobile',
-    'bird',
-    'cat',
-    'deer',
-    'dog',
-    'frog',
-    'horse',
-    'ship',
-    'truck'
-]
+class_names = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
 #LOSS FUNCTION
 def custom_loss(y_true, y_pred):
     ce_loss = tf.keras.losses.categorical_crossentropy(
-        y_true,
-        y_pred
+        y_true,y_pred
     )
     confidence_penalty = tf.reduce_mean(
         tf.square(y_pred)
     )
     total_loss = ce_loss + 0.1 * confidence_penalty
     return total_loss
-
 # CNN MODEL
 model = models.Sequential()
+model.add(
+    layers.Conv2D(
+        32,(3, 3),activation='relu',padding='same',input_shape=(32, 32, 3))
+)
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.25))
 
 model.add(
     layers.Conv2D(
-        32,
-        (3, 3),
-        activation='relu',
-        padding='same',
-        input_shape=(32, 32, 3)
+        64,(3, 3),activation='relu',padding='same'
     )
 )
 model.add(layers.BatchNormalization())
@@ -61,22 +48,7 @@ model.add(layers.Dropout(0.25))
 
 model.add(
     layers.Conv2D(
-        64,
-        (3, 3),
-        activation='relu',
-        padding='same'
-    )
-)
-model.add(layers.BatchNormalization())
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Dropout(0.25))
-
-model.add(
-    layers.Conv2D(
-        128,
-        (3, 3),
-        activation='relu',
-        padding='same'
+        128,(3, 3),activation='relu',padding='same'
     )
 )
 model.add(layers.BatchNormalization())
@@ -87,48 +59,33 @@ model.add(layers.Flatten())
 
 model.add(
     layers.Dense(
-        256,
-        activation='relu'
+        256,activation='relu'
     )
 )
 model.add(layers.Dropout(0.5))
 
 model.add(
     layers.Dense(
-        10,
-        activation='softmax'
+        10,activation='softmax'
     )
 )
 
 # COMPILE MODEL
 
 model.compile(
-    optimizer='adam',
-    loss=custom_loss,
-    metrics=['accuracy']
+    optimizer='adam',loss=custom_loss,metrics=['accuracy']
 )
-
 # MODEL SUMMARY
 model.summary()
-
 # TRAIN MODEL
 history = model.fit(
-    x_train,
-    y_train,
-    epochs=25,
-    batch_size=64,
-    validation_data=(x_test, y_test)
+    x_train,y_train,epochs=25,batch_size=64,validation_data=(x_test, y_test)
 )
-
 # EVALUATE MODEL
 loss, accuracy = model.evaluate(
-    x_test,
-    y_test,
-    verbose=1
+    x_test,y_test,verbose=1
 )
-print("\n================================")
-print("MODEL PERFORMANCE")
-print("================================")
+print("\nMODEL PERFORMANCE")
 print(f"Test Loss     : {loss:.4f}")
 print(f"Test Accuracy : {accuracy*100:.2f}%")
 
@@ -137,19 +94,13 @@ y_pred = np.argmax(predictions, axis=1)
 y_true = y_test_original.flatten()
 
 precision = precision_score(
-    y_true,
-    y_pred,
-    average='weighted'
+    y_true,y_pred,average='weighted'
 )
 recall = recall_score(
-    y_true,
-    y_pred,
-    average='weighted'
+    y_true,y_pred,average='weighted'
 )
 f1 = f1_score(
-    y_true,
-    y_pred,
-    average='weighted'
+    y_true,y_pred,average='weighted'
 )
 print("ADDITIONAL METRICS\n")
 print(f"Precision : {precision:.4f}")
@@ -160,9 +111,7 @@ print("\nCLASSIFICATION REPORT\n")
 
 print(
     classification_report(
-        y_true,
-        y_pred,
-        target_names=class_names
+        y_true,y_pred,target_names=class_names
     )
 )
 
@@ -181,17 +130,13 @@ for i in range(10):
 for i in range(5):
 
     plt.figure(figsize=(3,3))
-
     plt.imshow(x_test[i])
-
     plt.title(
         f"Actual: {class_names[y_true[i]]}\n"
         f"Predicted: {class_names[y_pred[i]]}"
     )
-
     plt.axis("off")
     plt.show()
-
 #graph
 plt.figure(figsize=(8,5))
 
@@ -199,7 +144,6 @@ plt.plot(
     history.history['accuracy'],
     label='Training Accuracy'
 )
-
 plt.plot(
     history.history['val_accuracy'],
     label='Validation Accuracy'
